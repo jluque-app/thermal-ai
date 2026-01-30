11ef78b21947590cba4bf4ac705a17b8e186cccf
Jaime Pérez Luque <jluque@escp.eu>
2026-01-30 07:30:21 +0100

    Debug: Add crash logging to main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
 
-import GlobalErrorBoundary from '@/components/GlobalErrorBoundary.jsx'
+console.log('--- FRONTEND BOOT STARTING ---');
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GlobalErrorBoundary>
-      <App />
-    </GlobalErrorBoundary>
-  </React.StrictMode>
-)
+import GlobalErrorBoundary from '@/components/GlobalErrorBoundary.jsx'
+try {
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <GlobalErrorBoundary>
+        <App />
+      </GlobalErrorBoundary>
+    </React.StrictMode>
+  )
+} catch (e) {
+  console.error("CRITICAL BOOT ERROR:", e);
+  document.getElementById('root').innerHTML = '<div style="color:red; padding:20px;"><h1>Critical Boot Error</h1><pre>' + e.toString() + '</pre></div>';
+}
